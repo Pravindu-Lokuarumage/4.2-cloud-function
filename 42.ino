701f07e45e00c634f586f4826d181a922abd3f63
Pravindu-Lokuarumage <65475307+Pravindu-Lokuarumage@users.noreply.github.com>
2020-06-02 00:54:34 +1000

    Add files via upload

---
+++ 42.ino
@@ -0,0 +1,52 @@
+int Red = D6;
+int Green = D5;
+int Blue = D4;
+
+void setup() {
+    
+    pinMode(Red, OUTPUT);
+    pinMode(Green, OUTPUT);
+    pinMode(Blue, OUTPUT);
+    
+    
+    Particle.function("toggleled", toggleled);
+    
+}
+
+int toggleled(String colour){
+    
+    if(colour == "red"){
+        
+        digitalWrite(Red, HIGH);
+        digitalWrite(Green, LOW);
+        digitalWrite(Blue, LOW);
+        return 1;
+        
+    }
+    
+    else if(colour == "blue"){
+        
+        digitalWrite(Blue, HIGH);
+        digitalWrite(Green, LOW);
+        digitalWrite(Red, LOW);
+        return 1;
+    }  
+    else if(colour == "green"){
+        
+        digitalWrite(Green, HIGH);
+        digitalWrite(Blue, LOW);
+        digitalWrite(Red, LOW);
+        return 1;
+        
+    }
+    else if(colour == "off"){
+        
+        digitalWrite(Blue, LOW);
+        digitalWrite(Green, LOW);
+        digitalWrite(Red, LOW);
+        return 1;
+    }
+    else{
+        return -1;
+    }
+}
